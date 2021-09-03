e2eadfcf276be7a646d829f794231c9099d1a8b3
AM KK <75850182+Endrju00@users.noreply.github.com>
2021-09-04 01:44:09 +0200

    Participants analysis has been added

---
+++ participants.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+if __name__ == '__main__':
+    athlete_events = pd.read_csv('athlete_events.csv')
+
+    data = []
+    for year in range(1896, 2017):
+        participants = athlete_events[athlete_events['Year'] == year]
+        participants = participants.drop_duplicates("ID", keep=False)
+        data.append((year, participants['Name'].count()))
+
+    # Plot
+    plt.style.use('seaborn-darkgrid')
+    fig, ax1 = plt.subplots(1, 1)
+    ax1.bar([x[0] for x in data], [x[1] for x in data])
+
+    ax1.set_title("Participants 1896-2016.", fontsize=16)
+    plt.show()
