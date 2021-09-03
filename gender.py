1a076fea71e2723e319e23093a46ff433f21f45a
AM KK <75850182+Endrju00@users.noreply.github.com>
2021-09-04 01:43:11 +0200

    Duplicates have been removed

--- gender.py
+++ gender.py
@@ -1,18 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 if __name__ == '__main__':
     athlete_events = pd.read_csv('athlete_events.csv')
     labels = ('Male', 'Female')
+    athlete_events_edited = athlete_events.drop_duplicates('ID') # to count gender the objects have to be unique
 
     male = athlete_events[athlete_events['Sex'] == 'M']
     female = athlete_events[athlete_events['Sex'] == 'F']
 
     # All participants
-    male_size = male['Sex'].count()
-    female_size = female['Sex'].count()
+    male_size = athlete_events_edited[athlete_events_edited['Sex'] == 'M']['Sex'].count()
+    female_size = athlete_events_edited[athlete_events_edited['Sex'] == 'F']['Sex'].count()
     sizes = [male_size, female_size]
-    explode = (0, 0.1)
 
     # Medals
     male_medals = male[pd.notna(male['Medal'])]['Medal'].count()
@@ -22,6 +23,7 @@ if __name__ == '__main__':
     # Plot
     plt.style.use('ggplot')
     fig, (ax1, ax2) = plt.subplots(1, 2)
+    explode = (0, 0.1)
 
     ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
             shadow=True, startangle=90)
