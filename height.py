37d9dcec08fc0a57a884eb9cf6fb0aa81c7f9fe4
AM KK <75850182+Endrju00@users.noreply.github.com>
2021-09-04 18:22:41 +0200

    Height of tall person has been changed

--- height.py
+++ height.py
@@ -13,16 +13,16 @@ if __name__ == '__main__':
     print(f'AVG MAN HEIGHT: {avg_man_height}, AVG WOMAN HEIGHT: {avg_woman_height}')
 
     # MEN
-    tall_men = male[athlete_events["Height"] >= 180]
-    short_men = male[athlete_events["Height"] < 180]
+    tall_men = male[athlete_events["Height"] >= 185]
+    short_men = male[athlete_events["Height"] < 185]
 
     # with medals
     tall_men = tall_men[pd.notna(tall_men['Medal'])]['Medal'].count()
     short_men = short_men[pd.notna(short_men['Medal'])]['Medal'].count()
 
     # WOMEN
-    tall_women = female[athlete_events["Height"] >= 170]
-    short_women = female[athlete_events["Height"] < 170]
+    tall_women = female[athlete_events["Height"] >= 175]
+    short_women = female[athlete_events["Height"] < 175]
 
     # with medals
     tall_women = tall_women[pd.notna(tall_women['Medal'])]['Medal'].count()
@@ -31,7 +31,7 @@ if __name__ == '__main__':
     # Plot
     plt.style.use('seaborn-darkgrid')
     fig, (ax1, ax2) = plt.subplots(1, 2)
-    ax1.bar(['Women (height < 170)', 'Tall women (height >= 170)'],
+    ax1.bar(['Women (height < 175)', 'Tall women (height >= 175)'],
             [short_women, tall_women,],
             color=['#e63946', '#023e8a'])
     ax1.set_title("Height and number of medals won by women comparision.",
@@ -39,7 +39,7 @@ if __name__ == '__main__':
     ax1.set_xlabel("Height")
     ax1.set_ylabel("Number of medals won")
 
-    ax2.bar(['Men (height < 180)', 'Tall men (height >= 180)'],
+    ax2.bar(['Men (height < 185)', 'Tall men (height >= 185)'],
             [short_men, tall_men],
             color=['#e63946', '#023e8a'])
 
